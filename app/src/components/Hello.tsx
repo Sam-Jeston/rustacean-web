c5cd930cb2c5a3a327ceba880454d80ef3dc36f7
Sam <sam.jeston@gmail.com>
2017-07-09 14:14:00 +1000

    Moved to Bulma CSS

--- app/src/components/Hello.tsx
+++ app/src/components/Hello.tsx
@@ -4,11 +4,54 @@ export interface HelloProps { compiler: string; framework: string; }
 
 // 'HelloProps' describes the shape of props.
 // State is never set so we use the 'undefined' type.
+
+// Add is is-active class next to navbar-menu to toggle for mobile
 export class Hello extends React.Component<HelloProps, undefined> {
-    render() {
-        return <div>
-          <h1>Hello from {this.props.compiler} and {this.props.framework}!</h1>
-          <pre>Woot</pre>
+  render() {
+    return (
+      <div>
+        <nav className="navbar is-transparent">
+          <div className="navbar-brand">
+            <a className="navbar-item" href="http://bulma.io">
+              <img src="http://bulma.io/images/bulma-logo.png" alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28" />
+            </a>
+
+            <div className="navbar-burger burger" data-target="navMenuExample">
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
+          </div>
+
+          <div id="navMenuExample" className="navbar-menu">
+            <div className="navbar-start">
+              <a className="navbar-item " href="http://bulma.io/">
+                Home
+              </a>
+            </div>
+
+            <div className="navbar-end">
+              <a className="navbar-item" href="https://github.com/Sam-Jeston/rustacean-web" target="_blank">
+                Github
+              </a>
+            </div>
+          </div>
+        </nav>
+        <div className="columns">
+          <div className="column">
+            First column
+          </div>
+          <div className="column">
+            Second column
+          </div>
+          <div className="column">
+            Third column
+          </div>
+          <div className="column">
+            Fourth column
+          </div>
         </div>
-    }
+      </div>
+    )
+  }
 }
--- app/webpack.config.js
+++ app/webpack.config.js
@@ -22,7 +22,7 @@ module.exports = {
             // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
             { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
             {
-              test: /\.scss$/,
+              test: /\.(scss|css|sass)$/,
               use: [
                 'style-loader',
                 'css-loader',
